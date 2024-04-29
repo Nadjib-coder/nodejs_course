ef169074eee22e63cb9fdaa3e88d845010a77b08
Nadjib-coder <nadjib.programmation1@gmail.com>
2024-04-29 15:11:30 +0100

    continius learning 29/05/2024

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log(module);
+const fs = require('fs');
+
+const files = fs.readdirSync('$');
+console.log('Current Directory', files);
+
+fs.readdir('../', (err, files) => {
+    if (err) {
+        console.error('Error', err);
+        return;
+    }
+    else {
+        console.log('Parent Directory', files);
+    }
+});
